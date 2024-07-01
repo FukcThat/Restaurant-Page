754aaf2f7936d08f943e3c76b38dada11b61d9e2
FukcThat <63652e636f6465@gmail.com>
2024-07-01 23:46:59 +0200

    Assignment Nr. 6: Delete hardcoded HTML Redo it in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log(Hello);
+const content = document.getElementById("content");
+const body = document.querySelector("body");
+
+// Headline
+const headline = document.createElement("h2");
+headline.textContent = "The Good Old Kitchen";
+
+// First paragraph
+const p1 = document.createElement("p");
+p1.textContent =
+  "The Good Old Kitchen is not really so much a restaurant as it is my kitchen. It is the welcoming refuge for all munchies. Meals, snacks, beverages and even treats can be made here. Meals are usually prepared once to twice a day. Self service available, provided you live shere.";
+
+// Second paragraph
+const p2 = document.createElement("p");
+p2.textContent =
+  "The Good Old Kitchen will hold all self service users responsible for restocking of emptied goods. Or so help me God...";
+
+content.appendChild(headline);
+content.appendChild(p1);
+content.appendChild(p2);
+body.appendChild(content);
