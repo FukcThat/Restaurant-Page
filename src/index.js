104a2cf295bd231e1f535a67d19645078fd80164
FukcThat <63652e636f6465@gmail.com>
2024-07-02 22:37:51 +0200

    STYLE: Made it a little less ugly

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ p1.textContent =
   "The Good Old Kitchen is not really so much a restaurant as it is my kitchen. It is the welcoming refuge for all munchies. Meals, snacks, beverages and even treats can be made here. Meals are usually prepared once to twice a day. Self service available, provided you live shere.";
 
 // Second paragraph
-const p2 = document.createElement("p");
-p2.textContent =
+const finePrint = document.createElement("p");
+finePrint.classList.add("smallText");
+finePrint.textContent =
   "The Good Old Kitchen will hold all self service users responsible for restocking of emptied goods. Or so help me God...";
 
 content.appendChild(headline);
 content.appendChild(p1);
-content.appendChild(p2);
+content.appendChild(finePrint);
 body.appendChild(content);
