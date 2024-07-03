2021327932481db0f3ee3595aac53aaa28c17079
FukcThat <63652e636f6465@gmail.com>
2024-07-03 17:53:31 +0200

    FEAT: Made MenuItem Array Added MenuItem Images

--- src/menuPageLoad.js
+++ src/menuPageLoad.js
@@ -3,6 +3,20 @@ export const menuPageLoad = document.addEventListener(
   () => {
     console.log("ok");
 
+    const menuItems = [
+      {
+        title: "Dish1",
+        description: "Something tasty.",
+        imgURL:
+          "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+      },
+      {
+        title: "Dish2",
+        description: "Something tasty also.",
+        imgURL: "https://ichef.bbci.co.uk/images/ic/480xn/p07ryyyj.jpg.webp",
+      },
+    ];
+
     const content = document.getElementById("content");
     const body = document.querySelector("body");
 
@@ -12,29 +26,40 @@ export const menuPageLoad = document.addEventListener(
 
     // Menu Item
 
-    const createMenuItem = () => {
+    const createMenuItem = (title, description, imgURL) => {
       const menuItem = document.createElement("div");
       menuItem.classList.add("menuItem");
 
       // MenuItem Title
       const menuItemTitle = document.createElement("h4");
-      menuItemTitle.textContent = "A tasty Thing";
+      menuItemTitle.textContent = title;
       menuItem.appendChild(menuItemTitle);
 
       // MenuItem Description
       const menuItemDesc = document.createElement("p");
-      menuItemDesc.textContent = "A tasty thing you might eat or drink.";
+      menuItemDesc.textContent = description;
       menuItem.appendChild(menuItemDesc);
 
       // Menu Image
-      const menuItemImg = document.createElement("image");
+      const menuItemImg = document.createElement("img");
+      menuItemImg.src = imgURL;
       menuItem.appendChild(menuItemImg);
 
       return menuItem;
     };
 
     content.appendChild(headline);
-    content.appendChild(createMenuItem());
+
+    for (let i = 0; i < menuItems.length; i++) {
+      content.appendChild(
+        createMenuItem(
+          menuItems[i].title,
+          menuItems[i].description,
+          menuItems[i].imgURL
+        )
+      );
+    }
+
     body.appendChild(content);
   }
 );
